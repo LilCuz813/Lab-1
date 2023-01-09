02f3a1af985f5e47cc3a3b0433859f1222da9be1
chris <chrisabelw@m3.com>
2023-01-09 16:05:15 -0500

    First Commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+
+Console.WriteLine("Welcome to Grand Circus' Room Detail Generator!");
+
+Console.WriteLine("Enter Length");
+double Length = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter Width");
+double Width = double.Parse(Console.ReadLine());
+
+double area = Length * Width;
+double perimeter = Length + Width;
+
+Console.WriteLine("Area " + Length * Width);
+
+Console.WriteLine("Perimeter " + Length + Width);
+
+if (area < 250)
+{
+    Console.WriteLine("The room is small.");
+
+
+}
+else if (area > 250 && area < 650)
+{
+    Console.WriteLine("The room is medium.");
+}
+else if (area > 650)
+{
+    Console.WriteLine("The room is large.");
+}
+
