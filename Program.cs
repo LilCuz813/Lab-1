5dc8433da5c6b6e35de9fa620dc6f87ed21ab663
chris <chrisabelw@m3.com>
2023-01-10 09:39:28 -0500

    updated lab 1

--- Program.cs
+++ Program.cs
@@ -11,24 +11,30 @@ double Length = double.Parse(Console.ReadLine());
 Console.WriteLine("Enter Width");
 double Width = double.Parse(Console.ReadLine());
 
+Console.WriteLine("Enter Height");
+double Height = double.Parse(Console.ReadLine());
+
 double area = Length * Width;
-double perimeter = Length + Width;
+double perimeter = 2 * (Length + Width);
+double volume = Length * Width * Height;
+double surfaceArea = 2 * ((Width * Length) + (Height * Length) * (Height * Width));
+Console.WriteLine("Area " + area);
 
-Console.WriteLine("Area " + Length * Width);
+Console.WriteLine("Perimeter " + perimeter);
 
-Console.WriteLine("Perimeter " + Length + Width);
+Console.WriteLine("Volume " + volume);
 
-if (area < 250)
+Console.WriteLine("Surface Area " + surfaceArea);
+
+if (area <= 250)
 {
     Console.WriteLine("The room is small.");
-
-
 }
 else if (area > 250 && area < 650)
 {
     Console.WriteLine("The room is medium.");
 }
-else if (area > 650)
+else if (area >= 650)
 {
     Console.WriteLine("The room is large.");
 }
